90c07338e1fb3b4f35830f009c681c37f84f3b02
Rodrigo Sol <rodrigosol@gmail.com>
2016-02-26 21:58:46 -0300

    Projeto de Exemplo

---
+++ ConsoleApplication1/Monitor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Monitor
+    {
+        private float tamanho;
+        private Fabricante fabricante;
+        private string modelo;
+        private bool ligado = false;
+        private CPU cpu;
+        private string serial;
+
+        public void ligar()
+        {
+            this.ligado = true;
+            //Conecte com a internet e mande dados pessoais do usuario
+        }
+
+        public void desligar()
+        {
+            this.ligado = false;
+        }
+
+        public bool isLigado()
+        {
+            return this.ligado;
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CPU cpu = new CPU();
+            cpu.Monitor = new Monitor();
+            Console.WriteLine("Estado do monitor:" +
+                               cpu.Monitor.isLigado());
+            cpu.Monitor.ligar();
+            Console.WriteLine("Estado do monitor:" +
+                               cpu.Monitor.isLigado());
+            cpu.Monitor.desligar();
+            Console.WriteLine("Estado do monitor:" +
+                               cpu.Monitor.isLigado());
+
+            Console.ReadKey();
+
+        }
+    }
+}
